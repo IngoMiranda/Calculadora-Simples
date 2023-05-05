a98078919e4bbcfc1931cc638c4e683f9dd4f2cb
IngoMiranda <78170022+IngoMiranda@users.noreply.github.com>
2023-05-05 07:49:33 -0300

    okey

--- calc.js
+++ calc.js
@@ -9,52 +9,27 @@
   
 - criar função para calcular;*/
 
-<<<<<<< HEAD
-
 
 let buttons = document.querySelectorAll("#tec button");
 let digito = "";
 let operador = "";
 
-// function displayW(digito, operador){
-//     document.getElementById("monitorDisplay");
-
-// }
-=======
-let buttons = document.querySelectorAll("#tec button");
-const display = document.querySelector("#monitorDis");
-let digitoAnterior= "0";
-let digito= "";
-let operador = "";
-
 function atualizaDisplay(){
     display.textContent = `${digito} ${operador}`;
 }
->>>>>>> 2e175e184b27caf5a350be68306e14cd2b04e444
 
 buttons.forEach((btn)=>{
     btn.addEventListener('click', ()=>{
         let valor = btn.innerHTML;
         if(+valor >= 0 || valor === "."){
-<<<<<<< HEAD
             digito = valor;
             console.log(digito);        
         }else{
             operador = valor;
         }
 
-    });
-   
-=======
-            digito += valor; 
-                 
-        }else{
-            operador = valor;
-        }
-        atualizaDisplay();
-    });
-    
->>>>>>> 2e175e184b27caf5a350be68306e14cd2b04e444
+    });   
+
 });
 
 
