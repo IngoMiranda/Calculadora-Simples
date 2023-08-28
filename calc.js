d88f987aea4a6a8ca0763c98a5d31ae0fe090af7
IngoMiranda <ingolima26@gmail.com>
2023-08-28 19:40:31 -0300

    :+1:

--- calc.js
+++ calc.js
@@ -19,50 +19,55 @@ const displayOPen = document.querySelector("#monitor");
 let digito = "";
 let operador = "";
 
-function displayAtualiza(digito){
-    display.innerHTML += digito; 
+function displayAtualiza(digito) {
+    display.innerHTML += digito;
     correcaoPonto();
-    
+    resultadoCalc();
+
 };
 
 // Eventos de clique
 
-buttons.forEach((btns)=>{
-    btns.addEventListener('click', ()=>{
+buttons.forEach((btns) => {
+    btns.addEventListener('click', () => {
         let btn = btns.innerHTML;
         displayAtualiza(btn);
     });
 });
 
-buttonC.addEventListener('click', ()=>{
+buttonC.addEventListener('click', () => {
     corrigirTudo();
 });
 
-buttonCe.addEventListener('click', ()=>{
+buttonCe.addEventListener('click', () => {
     let nada = "enfeite!";
     displayAtualiza(nada);
 });
 
-buttonDel.addEventListener('click', ()=>{
+buttonDel.addEventListener('click', () => {
     deletarUltimo();
 });
 
 // Funções correção e operações
 
-function corrigirTudo(){
-    display.innerHTML = " "; 
+function corrigirTudo() {
+    display.innerHTML = " ";
 };
 
-function deletarUltimo(){
+function deletarUltimo() {
     // slice gera uma nova matriz a partir de uma já existente. No caso abaixo uso o valor inical de 0 e o final de -1 
     // para que a sempre o ultimo valor da matriz seja excluído.
-    display.innerHTML = display.innerHTML.slice(0,-1);
+    display.innerHTML = display.innerHTML.slice(0, -1);
 };
 
-function correcaoPonto(entradaPonto){
+function correcaoPonto(entradaPonto) {
     //display.innerHTML = entradaPonto;
-    if(display.innerHTML.includes(".")){
+    if (display.innerHTML.includes(".")) {
         return;
     }
-    
+
+}
+
+function resultadoCalc() {
+
 }
