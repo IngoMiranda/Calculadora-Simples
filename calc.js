5763808754a1f460d642ad2441605b25e6808d65
IngoMiranda <ingolima26@gmail.com>
2023-09-17 16:41:53 -0300

    correção e contrução

--- calc.js
+++ calc.js
@@ -14,26 +14,52 @@ let buttons = document.querySelectorAll(".buttons");
 let buttonC = document.querySelector("#c");
 let buttonCe = document.querySelector("#ce");
 let buttonDel = document.querySelector("#del");
+let buttonEg = document.querySelector(".equal");
 const display = document.querySelector("#monitorDis");
 const displayOPen = document.querySelector("#monitor");
-let digito = "";
-let operador = "";
+let operador = document.querySelectorAll(".operador");
+let operadorAtual = "";
+let primeiroNum = "";
+let segundoNum = "";
 
-function displayAtualiza(digito) {
-    display.innerHTML += digito;
-    correcaoPonto();
+function displayAtualiza(valor) {
+    if (!Number(valor)) {
+        if (valor === "=") {
+            resultadoCalc()
+
+        } else {
+            operador += valor;
+        }
+
+    } else {
+        if (operador === " ") {
+            primeiroNum = valor;
+        }
+    }
+
+    display.innerHTML += valor;
+    correcaoPonto(valor);
+    corrigirTudo()
     resultadoCalc();
-
 };
 
 // Eventos de clique
 
 buttons.forEach((btns) => {
     btns.addEventListener('click', () => {
-        let btn = btns.innerHTML;
+        let btn = parseFloat(btns.innerHTML);
         displayAtualiza(btn);
     });
 });
+
+operador.forEach((btnOpera) => {
+    btnOpera.addEventListener('click', () => {
+        let operaBtn = btnOpera.innerHTML;
+        displayAtualiza(operaBtn);
+    });
+});
+
+buttonEg.addEventListener('click', );
 
 buttonC.addEventListener('click', () => {
     corrigirTudo();
@@ -60,7 +86,7 @@ function deletarUltimo() {
     display.innerHTML = display.innerHTML.slice(0, -1);
 };
 
-function correcaoPonto(entradaPonto) {
+function correcaoPonto() {
     //display.innerHTML = entradaPonto;
     if (display.innerHTML.includes(".")) {
         return;
--- tag.js
+++ tag.js
@@ -1,6 +1,6 @@
 document.body.onload = createTags();
- 
-function createTags(){
+
+function createTags() {
     var body = document.querySelector('body');
     var main = document.createElement('main');
     var display = document.createElement('div');
@@ -9,49 +9,48 @@ function createTags(){
     var monitorDisplay = document.createElement('p');
     const valueText = [
 
-        "CE", "C","DEL", "/", 7, 8, 9, "*", 4, 5, 6,"-", 1,
-        2, 3, "+", 0, ".","="   
+        "CE", "C", "DEL", "/", 7, 8, 9, "*", 4, 5, 6, "-", 1,
+        2, 3, "+", 0, ".", "="
     ];
     //criação de id e class
-    main.setAttribute("id","corpo");
-    teclas.setAttribute("id","tec");
-    display.setAttribute("id","display");
-    monitor.setAttribute("id","monitor");
-    monitorDisplay.setAttribute("id","monitorDis");
+    main.setAttribute("id", "corpo");
+    teclas.setAttribute("id", "tec");
+    display.setAttribute("id", "display");
+    monitor.setAttribute("id", "monitor");
+    monitorDisplay.setAttribute("id", "monitorDis");
 
-   // adicionando childs
+    // adicionando childs
     display.appendChild(monitor);
     display.appendChild(monitorDisplay);
     main.appendChild(display);
     main.appendChild(teclas);
     body.appendChild(main);
 
-    for( let i = 0; i < 19; i++ ){
+    for (let i = 0; i < 19; i++) {
 
         let buttons = document.createElement('button');
         buttons.setAttribute("class", "buttons");
         teclas.appendChild(buttons);
         buttons.innerHTML = valueText[i];
-        
-        if(valueText[i] === "CE" || valueText[i] === "C" || valueText[i] === "DEL"){
+
+        if (valueText[i] === "CE" || valueText[i] === "C" || valueText[i] === "DEL") {
             buttons.setAttribute('class', 'cor');
-            if(valueText[i] === "CE" ){
+            if (valueText[i] === "CE") {
                 buttons.setAttribute('id', 'ce');
-            }else if(valueText[i] === "C"){
+            } else if (valueText[i] === "C") {
                 buttons.setAttribute('id', 'c');
-            }else{
+            } else {
                 buttons.setAttribute('id', 'del');
-            }             
-        }else if(valueText[i] === "="){
+            }
+        } else if (valueText[i] === "=") {
             buttons.setAttribute('class', 'equal');
-                       
+
+        } else if (valueText[i] === "/" || valueText[i] === "*" || valueText[i] === "-" || valueText[i] === "+" || valueText[i] === ".") {
+            buttons.setAttribute('class', 'operador');
         };
-             
-    };   
+
+    };
 
 
-   
+
 }
-
-
-
