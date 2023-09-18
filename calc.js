b315ce684c3b023967c24791f663a988b69da8cf
IngoMiranda <ingolima26@gmail.com>
2023-09-18 07:14:23 -0300

    Atualização clique e verificação de valor

--- calc.js
+++ calc.js
@@ -14,10 +14,57 @@ let buttons = document.querySelectorAll(".buttons");
 let buttonC = document.querySelector("#c");
 let buttonCe = document.querySelector("#ce");
 let buttonDel = document.querySelector("#del");
-let buttonEg = document.querySelector(".equal");
-const display = document.querySelector("#monitorDis");
-const displayOPen = document.querySelector("#monitor");
-let operador = document.querySelectorAll(".operador");
+let buttonIgual = document.querySelector(".equal");
+let display = document.querySelector("#monitorDis");
+//const displayOPen = document.querySelector("#monitor");
+let operadores = document.querySelectorAll(".operador");
 let operadorAtual = "";
 let primeiroNum = "";
 let segundoNum = "";
+
+
+function displayAtualiza(valor) {
+    if (!Number(valor)) {
+        if (valor === "=") {
+            console.log("função");
+        } else {
+            operadorAtual = valor;
+        }
+    } else if (operadorAtual === "") {
+        primeiroNum += valor;
+    } else {
+        segundoNum += valor;
+    }
+
+    display.innerHTML += valor;
+
+};
+
+//Eventos 
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let btn = Number(button.innerHTML);
+        displayAtualiza(btn)
+    });
+});
+
+operadores.forEach((operadorButton) => {
+    operadorButton.addEventListener('click', () => {
+        let operadorBtn = operadorButton.innerHTML;
+        displayAtualiza(operadorBtn);
+    });
+});
+
+buttonC.addEventListener('click', () => {
+    display.innerHTML = "";
+    primeiroNum = "";
+    segundoNum = "";
+    operadorAtual = "";
+});
+
+buttonCe.addEventListener('click', () => {});
+
+buttonDel.addEventListener('click', () => {});
+
+buttonIgual.addEventListener('click', () => {});
